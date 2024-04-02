a045576b31d9897f74aad149613fc63c4c0ad19c
ahorner54 <ahorner589@gmail.com>
2024-04-01 21:39:28 -0500

    Registered models

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+#Added imports
+from .models import Book, Genre, BookInstance, Author
+
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
