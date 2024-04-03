1837ed4b2e38d71b60c644c73451ea49c00d4d27
ahorner54 <ahorner589@gmail.com>
2024-04-02 19:18:04 -0500

    Added permissions variable in Meta class of BookInstance model

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -94,6 +94,8 @@ class BookInstance(models.Model):
     class Meta:
         ordering = ['due_back']
 
+        permissions = (("can_mark_returned", "Set book as returned"),)
+
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
@@ -104,6 +106,7 @@ class BookInstance(models.Model):
     def is_overdue(self):
         """Determines if the book is overdue based on due date and current date."""
         return bool(self.due_back and date.today() > self.due_back)
+    
     
 class Author(models.Model):
     """Model representing an author."""
