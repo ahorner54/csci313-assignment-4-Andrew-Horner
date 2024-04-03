4f950520c004251dcc8c535f97a5e646bc95b3cb
ahorner54 <ahorner589@gmail.com>
2024-04-03 17:51:52 -0500

    Fixed errors and got all borrowed books page working

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -78,11 +78,11 @@ class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
     
-class LoanedBooksAllListView(PermissionRequiredMixin,generic.ListView):
+class LoanedBooksAllListView(PermissionRequiredMixin, generic.ListView):
     """Generic class-based view listing all books on loan."""
     model = BookInstance
-    'catalog.can_mark_returned'
-    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    permission_required = 'catalog.can_mark_returned'
+    template_name = 'catalog/bookinstance_list_borrowed_all.html'
     paginate_by = 10
 
     def get_queryset(self):
